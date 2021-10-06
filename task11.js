ee51cf3ff82e9dc15cae64ce6c844807215856c3
OlehYavoriv <oleh.yavoriv@gmail.com>
2021-10-06 14:21:11 +0300

    Training JS #11: loop statement --break,continue

---
+++ task11.js
@@ -0,0 +1,9 @@
+function grabDoll(dolls){
+    var bag=[];
+    for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+        bag.push(dolls[i]);
+      } else continue
+      if (bag.length >= 3) break}
+    return bag;
+  }
