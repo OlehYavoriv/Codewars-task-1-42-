a1d657fcd6e38cdd68a9dd575df293bb1e435b51
OlehYavoriv <oleh.yavoriv@gmail.com>
2021-10-06 14:21:52 +0300

    Training JS #12: loop statement --for..in and for..of

---
+++ task12.js
@@ -0,0 +1,13 @@
+function giveMeFive(obj){
+    var arr=[];
+    for (var key in obj){
+    if (key.length==5){
+      arr.push(key);
+      }
+    if (obj[key].length==5){
+      arr.push(obj[key]);
+    }
+      }
+    return arr;
+    
+  }
